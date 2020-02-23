a41f4250205bc0092150aacf4867099669c13052
Joanna Fatyga-Kaminska <jfatyga@wp.pl>
2020-02-23 13:05:43 +0100

    RomaConverter commit

---
+++ src/main/java/RomaConverter.java
@@ -0,0 +1,34 @@
+public class RomaConverter {
+
+    public int value (String v) {
+        switch (v) {
+            case "I":
+                return 1;
+            case "V":
+                return 5;
+            case "X":
+                return 10;
+            case "L":
+                return 50;
+            case "C":
+                return 100;
+            case "M":
+                return 1000;
+        }
+        return 0;
+    }
+
+    public int converter(String roma) {
+        String[] stringTable = roma.split("");
+        int result = 0;
+
+        for (int i = 0; i < stringTable.length; i++) {
+            boolean isAtEnd = i == stringTable.length - 1;
+            if (!isAtEnd && value(stringTable[i]) < value(stringTable[i+1])) {
+                result += value(stringTable[i]+1) - value(stringTable[i]);
+            } else {
+                result += value(stringTable[i]);
+            }
+        } return result;
+    }
+}
